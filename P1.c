51f9db3eda317af254181e6ad397153209ebb138
vicmaria <37628109+vicmaria@users.noreply.github.com>
2018-06-13 21:45:45 -0300

    Add files via upload

---
+++ P1.c
@@ -0,0 +1,58 @@
+//Nome: Victoria Maria Veloso Rodrigues         Matricula: 11811ECP003
+
+#include <stdio.h>
+
+int main()
+{
+	int estado = 0, i = 0;
+	char bits[256];
+	
+	printf("Digite um numero binario: ");
+	scanf("%s", &bits);
+	
+	while(bits[i] != '\0')
+	{
+		if(estado == 0 && bits[i] == '0' )
+			 estado = 0;
+		else
+		{
+		if(estado == 0 && bits[i] == '1' )
+		  estado = 1;
+		else
+		{
+		if(estado == 1 && bits[i] == '0' )
+		  estado = 2;
+		else
+		{
+		if(estado == 1 && bits[i] == '1' )
+		  estado = 0;
+		else
+		{
+		if(estado == 2 && bits[i] == '0' )
+		  estado = 1;
+		else
+		{
+		if(estado == 2 && bits[i] == '1' )
+		  estado = 2;
+	    }
+	    }
+	    }
+	    } 
+        }
+	  
+	  i = i + 1;
+	  
+	}
+
+	if(estado == 0)
+	{
+		printf(" \"%s\" e multiplo de 3", bits);
+	}
+	else 
+	{
+		printf(" \"%s\" nao e", bits);
+	}
+	
+	
+	return 0;
+}
